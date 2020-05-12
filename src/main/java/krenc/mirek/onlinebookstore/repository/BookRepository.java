0bd511ebdb626bbeea661f442a833a55a6901655
kremir <kremir@o2.pl>
2020-05-12 18:57:44 +0200

    Some changes to Repository

--- src/main/java/krenc/mirek/onlinebookstore/repository/BookRepository.java
+++ src/main/java/krenc/mirek/onlinebookstore/repository/BookRepository.java
@@ -3,7 +3,9 @@ package krenc.mirek.onlinebookstore.repository;
 import krenc.mirek.onlinebookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
